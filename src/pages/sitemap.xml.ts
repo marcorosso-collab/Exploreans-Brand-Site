cdc5336651659686145e02e1063af85e37e57a02
Franco Agent <franco@planhotel.com>
2026-07-28 15:05:50 +0200

    fix: sitemap trailing slash, remove robots.txt crawl-delay
    - sitemap.xml.ts: ensure homepage URL uses trailing slash to match canonical
    - robots.txt: remove Crawl-delay directive (not honoured by Google, can cause issues)
    
    Improves GSC coverage for exploreans.com redirect issues

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -10,7 +10,7 @@ export const GET: APIRoute = async () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(page => `  <url>
-    <loc>${siteUrl}${page.url === '/' ? '' : page.url}/</loc>
+    <loc>${siteUrl}${page.url === '/' ? '/' : page.url + '/'}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
